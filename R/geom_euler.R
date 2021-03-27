1221b94bb6465b43e27335590e1ac1444e471b4c
Nicolas Huber <50340626+NicolasH2@users.noreply.github.com>
2021-03-27 22:03:16 +0100

    Update geom_euler.R

--- R/geom_euler.R
+++ R/geom_euler.R
@@ -1,3 +1,35 @@
+# devtools::document()
+
+#' create a venn diagram in ggplot2
+#'
+#' Imports:
+#' ggplot2
+#'
+#' @inheritParams ggplot2::geom_polygon
+#' @inheritParams ggplot2::geom_text
+#' @import ggplot2
+#'
+#' @param setlist list of character vectors.
+#' @param textsize integer, defining the text size of the numbers in the graph.
+#' @param xlim vector with 2 numbers, x axis limits for the venn diagram.
+#' @param ylim vector with 2 numbers, y axis limits for the venn diagram.
+#' @param fillCol boolean, should the fill color indicate the list items?
+#' @param borderCol boolean, should the border color indicate the list items?
+#' @param fixedCoords boolean, should coord_fixed be applied? If TRUE, makes the graph into a square.
+#' @return a ggplot2 layer objects (geom_polyon for the venn diagram) that can directly be added to a ggplot2 object
+#' @details the function uses geom_polygon for the dendrogram, so ... takes all arguements that geom_polygon would also take, such as color, size, alpha, etc.
+#' @export
+#' @examples
+#' library(ggeuler)
+#' library(ggplot2)
+#'
+#' set1 <- rownames(subset(mtcars, mpg>18))
+#' set2 <- rownames(subset(mtcars, qsec>18))
+#' set3 <- rownames(subset(mtcars, cyl<5))
+#' carset <- list(highMpg=set1, highQsec=set2, lowCyl=set3)
+#'
+#' ggplot2::ggplot() + geom_euler(carset)
+#'
 geom_euler <- function(setlist, xlim=c(0,1), ylim=c(0,1), numbersize=4, numbercolor="black", textsize=4, textcolor="black", fillCol=TRUE, borderCol=FALSE, fixedCoords=TRUE, ...){
   # find overlap between sets, using a ggeuler function
   overlap <- ggeuler::findOverlap(setlist, xlim=xlim, ylim=ylim)
